b5f7f672407669a285bd2119dfd4c921bf8db774
Mike Chau <mike@chau.io>
2015-12-25 00:13:09 -0600

    Remove references to sri plugin

--- index.js
+++ index.js
@@ -5,42 +5,36 @@ var CustomStats = require('webpack-custom-stats-patch');
 
 var DEFAULT_PARAMS = {
   customStatsKey: 'sprockets',
-  sriKey: 'integrity'
+  ignore: /\.(gz|html)$/
 };
 
 function SprocketsStatsWebpackPlugin(options) {
   var params = options || {};
-  var subresourceIntegrity = (params.subResourceIntegrity === undefined ? true : params.subResourceIntegrity);
 
-  this._customStatsKey = options.customStatsKey || 'sprockets';
-  this._sriKey = params.sriKey || 'integrity';
-
-  if (subresourceIntegrity) {
-    try {
-      var sriPlugin = require('@mikechau/sri-webpack-plugin');
-      this._sriEnabled = true;
-    } catch(err) {
-      this._sriEnabled = false;
-    }
-  }
-};
+  this._customStatsKey = options.customStatsKey || DEFAULT_PARAMS.customStatsKey;
+  this._ignore = params.ignore || DEFAULT_PARAMS.ignore;
+}
 
 SprocketsStatsWebpackPlugin.prototype.apply = function(compiler) {
   var customStatsKey = this._customStatsKey;
-  var sriKey = this._sriKey;
-  var sriEnabled = this._sriEnabled;
+  var blacklistRegex = this._ignore;
   var sprockets = {};
 
   compiler.plugin('this-compilation', function(compilation) {
     compilation.plugin('optimize-assets', function(assets, callback) {
       Object.keys(assets).forEach(function(file) {
-        var asset = assets[file];
-        var content = asset.source();
+        var asset;
+        var content;
 
-        sprockets[file] = {
-          size: asset.size(),
-          digest: crypto.createHash('md5').update(content).digest('hex')
-        };
+        if (file.match(!blacklistRegex)) {
+          asset = assets[file];
+          content = asset.source();
+
+          sprockets[file] = {
+            size: asset.size(),
+            digest: crypto.createHash('md5').update(content).digest('hex')
+          };
+        }
       });
 
       callback();
