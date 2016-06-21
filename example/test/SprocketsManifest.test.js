d4702aab65d16bdeda43fd3da34951973556fb4b
Mike Chau <mike@chau.io>
2016-06-21 03:35:06 -0500

    Refactor example test to ava test

--- example/test/SprocketsManifest.test.js
+++ example/test/SprocketsManifest.test.js
@@ -1,17 +1,19 @@
-var assert = require('power-assert');
+'use strict';
+
+var test = require('ava');
 var actualManifest = require('../build/sprockets-manifest');
 var expectedManifest = require('./fixtures/sprockets-manifest');
 
-var actualFiles = actualManifest.files;
+test('generated sprockets manifest matches expected', function(t) {
+  var actualFiles = actualManifest.files;
 
-Object.keys(actualFiles).forEach(function(filename) {
-  var file = actualFiles[filename];
+  Object.keys(actualFiles).forEach(function(filename) {
+    var file = actualFiles[filename];
 
-  assert.ok(Date.parse(file.mtime), 'not a valid date');
+    t.truthy(Date.parse(file.mtime), 'not a valid date');
 
-  file.mtime = '2016-06-21T07:42:09.623Z';
+    file.mtime = '2016-06-21T07:42:09.623Z';
+  });
+
+  t.deepEqual(actualManifest, expectedManifest, 'manifests not equal');
 });
-
-assert.deepEqual(actualManifest, expectedManifest, 'manifests not equal');
-
-console.log('Test passed!');
