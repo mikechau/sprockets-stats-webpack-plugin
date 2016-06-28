45f1bc2f69ec5237eeca8e86dbe260bc8eb43da8
Mike Chau <mike@chau.io>
2016-06-27 19:09:23 -0500

    Remove test:all npm script

--- test/integration/simple.test.js
+++ test/integration/simple.test.js
@@ -5,17 +5,11 @@ var webpack = require('webpack');
 var path = require('path');
 var MemoryFS = require("memory-fs");
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var SriStatsPlugin = require("sri-stats-webpack-plugin");
-var SprocketsStatsPlugin = require("../../");
-
 var config = require('./scenarios/simple/webpack.config');
 
 test.cb('generated sprockets manifest matches expected', function(t) {
-  // var tmpDir = tmp.dirSync();
   var fs = new MemoryFS();
   var tmpDir = { name: path.resolve(__dirname, '../../tmp/01-test') };
-  var webpackDir = '/home/mike/code/github/sprockets-stats-webpack-plugin/tmp/01-test';
 
   var compilier = webpack(config(tmpDir.name));
   compilier.outputFileSystem = fs;
